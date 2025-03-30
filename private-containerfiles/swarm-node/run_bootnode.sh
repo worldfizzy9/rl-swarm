6a074b5b4b2f70976ed50e4573e23585b718c8aa
Christopher Nies <christopher@gensyn.ai>
2025-03-30 09:41:11 -0700

    Adding new script

---
+++ private-containerfiles/swarm-node/run_bootnode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#General args
+ROOT=$PWD
+
+export IDENTITY_PATH
+
+# Path to an RSA private key. If this path does not exist, a new key pair will be created.
+# Remove this file if you want a new PeerID.
+DEFAULT_IDENTITY_PATH="$ROOT"/swarm.pem
+IDENTITY_PATH=${IDENTITY_PATH:-$DEFAULT_IDENTITY_PATH}
+
+#lets go!
+echo "Getting requirements..."
+
+if ! which nvidia-smi; then
+   #You don't have a NVIDIA GPU
+   CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
+elif [ -n "$CPU_ONLY" ]; then
+   # ... or we don't want to use it
+   CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
+else
+   #NVIDIA GPU found
+   pip install -r "$ROOT"/requirements_gpu.txt
+   CONFIG_PATH="$ROOT/hivemind_exp/configs/gpu/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
+fi
+
+echo ">> Done!"
+echo ""
+echo ""
+echo "Good luck in the swarm!"
+
+python -m hivemind_exp.gsm8k.train_single_gpu \
+    --identity_path "$IDENTITY_PATH" \
+    --wallet_private_key "$WALLET_PRIVATE_KEY" \
+    --config "$CONFIG_PATH"
